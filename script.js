fd4121005f4265a8e73d655cd1c1c0bef2d5b208
chetanchrry008 <chetancherry008@gmail.com>
2025-03-23 19:16:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById("extra_tag1").onclick=function () {
+    var info=document.getElementById("html_hidn1");
+    info.style.display="block";
+}
+document.getElementById("hide_btn1").onclick=function () {
+    var infoo=document.getElementById("html_hidn1");
+    infoo.style.display="none";
+}
+
+
+document.getElementById("extra_tag2").onclick=function () {
+    var info2=document.getElementById("html_hidn2");
+    info2.style.display="block";
+}
+document.getElementById("hide_btn2").onclick=function () {
+    var infoo2=document.getElementById("html_hidn2");
+    infoo2.style.display="none";
+}
+
+document.getElementById("extra_tag3").onclick=function () {
+    var info2=document.getElementById("html_hidn3");
+    info2.style.display="block";
+}
+document.getElementById("hide_btn3").onclick=function () {
+    var infoo2=document.getElementById("html_hidn3");
+    infoo2.style.display="none";
+}
+
+
+document.getElementById("extra_tag4").onclick=function () {
+    var info2=document.getElementById("html_hidn4");
+    info2.style.display="block";
+}
+document.getElementById("hide_btn4").onclick=function () {
+    var infoo2=document.getElementById("html_hidn4");
+    infoo2.style.display="none";
+}
